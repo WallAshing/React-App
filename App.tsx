b6f94cf5c2bb8aea8321849851d997057d1369bd
Alexandre <alexandre.vauthier@edu.devinci.fr>
2021-06-26 22:52:31 +0200

    setState the cityName value et keep it for the temp page

--- App.tsx
+++ App.tsx
@@ -1,76 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import Style from './src/Style';
+import React, { Component} from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
-let city = "";
+export default class ReactExercice extends Component {
 
-export default function App() {
-  return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <Router>
-	<div>	
-    <nav>
-		<ul>
-			<li>
-			<Link to="/">Home</Link>
-			</li>
-			<li>
-			<Link to="/temp">Temp</Link>
-			</li>
-		</ul>
-    </nav>
-    	<Switch>
-			<Route exact path="/" component={Home}></Route>
-			<Route exact path="/temp" component={Temp}></Route>
-      	</Switch>
-	</div>
-	</Router>
-  );
+	constructor(props?: any){
+		super(props)
+		this.state = {
+			cityName: ""
+		}
+	}
+
+	render(){
+		return (
+			<Router>
+				<div>	
+					<nav>
+						<ul>
+							<li>
+							<Link to="/">Home</Link>
+							</li>
+							<li>
+							<Link to="/temp">Temp</Link>
+							</li>
+						</ul>
+					</nav>
+					<Switch>
+						<Route exact path="/">
+							<View style={Style.container}>
+								<Text style={Style.title}>Entrez le nom de la ville :</Text>
+								<TextInput 
+								style={Style.input}
+								placeholder='Enter city name...' 
+								onChangeText={(val) => this.setState({cityName: val})} 
+								/>
+								<Text style={Style.title}>Le text est le suivant : {this.state.cityName}</Text>
+							</View>
+						</Route>
+						<Route exact path="/temp">
+							<h2> Il fait cette température :</h2>
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
 }
-
-function Home() {
-	return(
-		<View style={styles.container}>
-			<Text style={styles.title}>Entrez le nom de la ville :</Text>
-			<TextInput style={styles.input} textContentType="addressCityAndState"/>
-			<Button title="Press me" onPress={() => window.alert('Saved')}/>
-	  	</View>
-	)
-	
-}
-
-function Temp() {
-	return(
-		<h2> Il fait cette température :</h2>
-		
-	)
-}
-
-
-
-const styles = StyleSheet.create({
-  	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginHorizontal: 16,
-  	},
-  	title: {
-		textAlign: 'center',
-		marginVertical: 8,
-	},
-	input: {
-		height: 40,
-		margin: 12,
-		padding: 12,
-		borderWidth: 1,
-		borderRadius: 5,
-	},
-});
-
-
---
+++ src/Style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+var Style = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginHorizontal: 16,
+    },
+    title: {
+      textAlign: 'center',
+      marginVertical: 8,
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      padding: 12,
+      borderWidth: 1,
+      borderRadius: 5,
+    },
+});
+
+export default Style;
